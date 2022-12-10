b3426826886acc9f56d1d4d42dce3aece97513cc
camilo ardila <miloardila98@gmail.com>
2022-12-09 23:21:40 -0500

    leyendo los datos agregados al carrito

---
+++ js/app.js
@@ -0,0 +1,40 @@
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarrito = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListener();
+
+function cargarEventListener() {
+    // cuando agregar un curso presionando agregar al carrito
+    listaCursos.addEventListener('click', agregarCurso);
+}
+
+//funciones
+
+function agregarCurso(e) {
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')){
+        const cursoSeleccionado = e.target.parentElement.parentElement
+        leerDatosCurso(cursoSeleccionado); 
+    }
+}
+
+// lee el contenido del html 
+
+function leerDatosCurso(curso){
+
+    console.log(curso);
+
+    // crear un objeto con el contenido del curso actual
+
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+    
+    console.log(infoCurso);
+}
